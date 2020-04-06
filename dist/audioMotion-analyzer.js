eb5a510d58104cae960bf6e69581c9e6211a6c77
Henrique Vianna <hvianna@gmail.com>
2020-04-06 13:58:06 -0300

    Version bump for 2.1.0 release.

--- dist/audioMotion-analyzer.js
+++ dist/audioMotion-analyzer.js
@@ -1,13 +1,13 @@
-/**
+/*!
  * audioMotion-analyzer
  * High-resolution real-time graphic audio spectrum analyzer JS module
  *
- * @version 2.0.0
+ * @version 2.1.0
  * @author  Henrique Avila Vianna <hvianna@gmail.com> <https://henriquevianna.com>
  * @license AGPL-3.0-or-later
  */
 
-const _VERSION = '2.0.0';
+const _VERSION = '2.1.0';
 
 export default class AudioMotionAnalyzer {
 
