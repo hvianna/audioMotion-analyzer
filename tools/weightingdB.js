3dde45597ff92230319244603938a821c166cf16
Henrique Vianna <hvianna@gmail.com>
2022-12-04 18:03:47 -0300

    Add graphs to the filters viewer tool; tools' CSS cleanup.

--- tools/weightingdB.js
+++ tools/weightingdB.js
@@ -35,7 +35,7 @@ function weightingdB( freq, filter ) {
 				  rD = ( freq / 6.8966888496476e-5 ) * Math.sqrt( h / ( ( f2 + 79919.29 ) * ( f2 + 1345600 ) ) );
 			return linearTodB( rD );
 
-		case 'I' : // ITU-R 468 https://en.wikipedia.org/wiki/ITU-R_468_noise_weighting
+		case '468' : // ITU-R 468 https://en.wikipedia.org/wiki/ITU-R_468_noise_weighting
 			const h1 = -4.737338981378384e-24 * freq ** 6 + 2.043828333606125e-15 * freq ** 4 - 1.363894795463638e-7 * f2 + 1,
 				  h2 = 1.306612257412824e-19 * freq ** 5 - 2.118150887518656e-11 * freq ** 3 + 5.559488023498642e-4 * freq,
 				  rI = 1.246332637532143e-4 * freq / Math.sqrt( h1 ** 2 + h2 ** 2 );
