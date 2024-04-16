4243ee6625b3d5054627a968af63d516234bccc9
Henrique Vianna <hvianna@gmail.com>
2024-04-16 19:51:26 -0300

    Tests with UMD module generation.

--- src/audioMotion-analyzer.js
+++ src/audioMotion-analyzer.js
@@ -2,12 +2,12 @@
  * audioMotion-analyzer
  * High-resolution real-time graphic audio spectrum analyzer JS module
  *
- * @version 4.4.0
+ * @version 4.5.0-beta.0
  * @author  Henrique Avila Vianna <hvianna@gmail.com> <https://henriquevianna.com>
  * @license AGPL-3.0-or-later
  */
 
-const VERSION = '4.4.0';
+const VERSION = '4.5.0-beta.0';
 
 // internal constants
 const PI      = Math.PI,
