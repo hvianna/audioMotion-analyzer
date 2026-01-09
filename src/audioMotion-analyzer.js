d76492c2680fefd7760f28185adfcfa96969cf63
Henrique Vianna <hvianna@gmail.com>
2026-01-09 14:14:43 -0300

    version bump; update changelog

--- src/audioMotion-analyzer.js
+++ src/audioMotion-analyzer.js
@@ -2,12 +2,12 @@
  * audioMotion-analyzer
  * High-resolution real-time graphic audio spectrum analyzer JS module
  *
- * @version 4.5.3
+ * @version 4.5.4
  * @author  Henrique Avila Vianna <hvianna@gmail.com> <https://henriquevianna.com>
  * @license AGPL-3.0-or-later
  */
 
-const VERSION = '4.5.3';
+const VERSION = '4.5.4';
 
 // internal constants
 const PI      = Math.PI,
